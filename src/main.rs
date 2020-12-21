fe53e82a6ae422de0f783eb42707eca43bc414c4
xh-gotz9 <xh353127981@gmail.com>
2020-12-22 00:32:19 +0800

    test: 向外部 node 发送 `get_peers` query, 并接收返回消息

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,12 @@ use std::{collections::BTreeMap, rc::Rc};
 use node::id::NodeID;
 
 // router.utorrent.com
-const DHT_BOOT_NODE_IP: &str = "82.221.103.244";
-const DHT_BOOT_NODE_PORT: u16 = 6881;
+const DHT_BOOT_NODE_IP: &str = "135.216.109.124";
+const DHT_BOOT_NODE_PORT: u16 = 8526;
 
 fn main() -> std::io::Result<()> {
-    let id = NodeID::rand();
-    let target_id = NodeID::rand();
+    let mut id = NodeID::rand();
+    let mut target_id = NodeID::rand();
     println!("{:?}", id);
     let mut v = BTreeMap::new();
     let t: u16 = rand::random();
@@ -22,26 +22,28 @@ fn main() -> std::io::Result<()> {
         BData::BString(hex::encode([(t >> 8) as u8, t as u8])),
     );
     v.insert(String::from("y"), BData::BString(String::from("q")));
-    v.insert(String::from("q"), BData::BString(String::from("find_node")));
+    v.insert(String::from("q"), BData::BString(String::from("get_peers")));
 
     // "a" args
     let mut args = BTreeMap::new();
-    args.insert(String::from("id"), BData::BString(id.to_string()));
+    args.insert(String::from("id"), BData::BString(id.raw_id()));
     args.insert(
-        String::from("target"),
-        BData::BString(target_id.to_string()),
+        String::from("info_hash"),
+        BData::BString(target_id.raw_id()),
     );
     v.insert(String::from("a"), BData::Dict(Rc::new(args)));
 
     let dict = BData::Dict(Rc::new(v));
 
     let data = fraux_rs::stringify(&dict).expect("stringify err");
+
     // 发送
     let node_addr = SocketAddrV4::new(
         DHT_BOOT_NODE_IP.parse().expect("node addr parse failed"),
         DHT_BOOT_NODE_PORT,
     );
-    println!("request:\n{}", data);
+
+    println!("request:\n{}", hex::encode(data.as_bytes()));
 
     let socket = UdpSocket::bind("0.0.0.0:9000")?;
     socket.send_to(data.as_bytes(), node_addr)?;
@@ -50,11 +52,14 @@ fn main() -> std::io::Result<()> {
     let mut count = 0;
     while count < 1 {
         let (len, addr) = socket.recv_from(&mut buffer)?;
+        println!("received data: {} bytes", len);
         let buffer = &buffer[..len];
         if addr.eq(&SocketAddr::V4(node_addr)) {
             let response =
                 String::from_utf8(buffer.to_vec()).expect("response parse string failed");
-            println!("response:\n{}", response);
+            let res =
+                fraux_rs::parse(&response).unwrap_or(BData::BString("parse failed".to_string()));
+            println!("response:\n{:?}", res);
             count += 1;
         }
     }
