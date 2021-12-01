5fbae5aa08cfcfa888b2336799a675a55d639975
xh-gotz9 <xh353127981@gmail.com>
2021-12-01 17:20:54 +0800

    adjust KademliaTable data structure etc.
    - KademliaTable.buckets wrapped by RefCell
    - `find_bucket_to_insert` return bucket index info

--- src/kademlia.rs
+++ src/kademlia.rs
@@ -9,32 +9,31 @@ use crate::{
 type BucketRef = Rc<RefCell<Bucket>>;
 
 pub struct KademliaTable {
-    buckets: Vec<BucketRef>,
+    buckets: RefCell<Vec<BucketRef>>,
 }
 
 impl KademliaTable {
     pub fn new() -> KademliaTable {
         return KademliaTable {
-            buckets: vec![Rc::new(RefCell::new(Bucket::new(MIN_HASH, MAX_HASH)))],
+            buckets: RefCell::new(vec![Rc::new(RefCell::new(Bucket::new(MIN_HASH, MAX_HASH)))]),
         };
     }
 
     /// # TODO
     /// splite when bucket is full
     pub fn insert_node(&self, node: Node) {
-        if let Some(b) = self.find_bucket_to_insert(node.id) {
-            b.as_ref().borrow_mut().insert(node);
+        if let Some((i, b)) = self.find_bucket_to_insert(node.id) {
         } else {
             panic!("not found bucket of {:?}", node.id);
         }
     }
 
-    fn find_bucket_to_insert(&self, id: NodeID) -> Option<BucketRef> {
-        for ele in &self.buckets {
+    fn find_bucket_to_insert(&self, id: NodeID) -> Option<(usize, BucketRef)> {
+        for (i, ele) in self.buckets.borrow().iter().enumerate() {
             let b = Rc::clone(&ele);
 
             if b.as_ref().borrow().node_in_range(id) {
-                return Some(b);
+                return Some((i, b));
             }
         }
 
