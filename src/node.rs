17f9c3d4b97e31f52c03a3784ea9e52515c6277a
xh-gotz9 <xh353127981@gmail.com>
2022-01-14 15:27:13 +0800

    add `addr` field into `Node`
    - rewrite Node::random and Node::new

--- src/node.rs
+++ src/node.rs
@@ -1,29 +1,39 @@
 use std::{
     fmt::{self, Debug, Result},
+    net::{IpAddr, Ipv4Addr, SocketAddr},
     time::SystemTime,
 };
 
 use chrono::{DateTime, Local};
+use rand::Rng;
 
 pub type NodeID = crate::hash::Hash;
 
 pub struct Node {
     pub id: NodeID,
+    pub addr: SocketAddr,
     pub last_active: SystemTime,
 }
 
 const GOOD_NODE_DURATION_SECONDS: u64 = 60 * 15;
 
 impl Node {
-    pub fn new(id: NodeID, last_active: SystemTime) -> Self {
-        Self { id, last_active }
+    pub fn new(id: NodeID, addr: SocketAddr, last_active: SystemTime) -> Self {
+        Self {
+            id,
+            addr,
+            last_active,
+        }
     }
 
     pub fn random() -> Node {
-        return Node {
+        let mut rd = rand::thread_rng();
+
+        Self {
             id: NodeID::random(),
+            addr: SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), rd.gen()),
             last_active: SystemTime::now(),
-        };
+        }
     }
 
     pub fn is_good(&self) -> bool {
